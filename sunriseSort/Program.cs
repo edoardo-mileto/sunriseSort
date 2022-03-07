136aadbefeaebe8807583bcd5a12b0116166da7b
Edoardo <edomileto@gmail.com>
2022-03-08 00:59:08 +0100

    Add graphic component using spectre.console to visualize sorting process

--- sunriseSort/Program.cs
+++ sunriseSort/Program.cs
@@ -1,22 +1,83 @@
-﻿using static sunriseSort.SunriseSort;
+﻿using Spectre.Console;
+using static sunriseSort.SunriseSort;
 
-Random rand = new Random();
+AnsiConsole.Write(
+    new FigletText("Sunrise sort")
+        .Centered()
+        .Color(Color.Yellow));
 
-int[] array = new int[16];
+string[] controllore =
+{
+    "[maroon on maroon]11[/]",
+    "[green on green]11[/]",
+    "[olive on olive]11[/]",
+    "[navy on navy]11[/]",
+    "[purple on purple]11[/]",
+    "[teal on teal]11[/]",
+    "[silver on silver]11[/]",
+    "[red on red]11[/]",
+    "[lime on lime]11[/]",
+    "[yellow on yellow]11[/]",
+    "[blue on blue]11[/]",
+    "[fuchsia on fuchsia]11[/]",
+    "[aqua on aqua]11[/]",
+    "[white on white]11[/]",
+    "[darkturquoise on darkturquoise]11[/]",
+    "[hotpink on hotpink]11[/]",
+};
 
-//il contenuto dell'array è generato in maniera random con interi compresi tra 0 e 100 
-for (int i = 0; i < array.Length; i++)
-    array[i] = rand.Next(0, 100);
 
-Console.WriteLine("Unsorted");
-foreach(var num in array){
-    Console.Write($"{num} ");
+(int, string)[] tuplesArray = new (int, string)[16];
+Random rd = new Random();
+
+for (int i = 0; i < 16; i++)
+{
+    tuplesArray[i].Item1 = rd.Next(0, 100);
+    tuplesArray[i].Item2 = controllore[i];
 }
 
-Console.WriteLine("\n\nSorted");
+var numbers = tuplesArray.Select(x => x.Item1).ToArray();
 
-Sort(array);
+string[] result = numbers.Select(x => x.ToString()).ToArray();
 
-foreach(var num in array){
-    Console.Write($"{num} ");
-}
+
+var table = new Table().Centered();
+
+AnsiConsole.Live(table)
+    .Start(ctx =>
+    {
+        table.Title("[Red bold] Unsorted [/]");
+        for (int i = 0; i < 16; i++)
+        {
+            table.AddColumn(new TableColumn(new Markup($"{tuplesArray[i].Item2}")));
+            table.Columns[i].Width(2);
+            ctx.Refresh();
+            Thread.Sleep(100);
+        }
+
+        table.AddRow(result);
+    });
+
+Sort(tuplesArray);
+
+var numbersSorted = tuplesArray.Select(x => x.Item1).ToArray();
+
+string[] resultSorted = numbersSorted.Select(x => x.ToString()).ToArray();
+
+
+var tableSorted = new Table().Centered();
+
+AnsiConsole.Live(tableSorted)
+    .Start(ctx =>
+    {
+        tableSorted.Title("[Green bold] Sorted [/]");
+        for (int i = 0; i < 16; i++)
+        {
+            tableSorted.AddColumn(new TableColumn(new Markup($"{tuplesArray[i].Item2}")));
+            tableSorted.Columns[i].Width(2);
+            ctx.Refresh();
+            Thread.Sleep(100);
+        }
+
+        tableSorted.AddRow(resultSorted);
+    });
